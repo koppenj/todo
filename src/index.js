e34ebba505d745f0e17df6bbdea421261056d7cc
koppenj <joshuwakoppen@gmail.com>
2022-03-19 13:28:16 -0400

    Refreshing workspace

--- src/index.js
+++ src/index.js
@@ -2,3 +2,9 @@ import './styles.css';
 
 console.log('Hello There General Kenobi');
 
+// At page load, check localstorge for a saved to do list
+// Should probably remove save/load from action list and
+// turn it into a single button/header nav bar
+// Populate saved to do in blue sidebar
+// Red box will populate when an action is highlighted?
+//Need to build out constructor for actions
