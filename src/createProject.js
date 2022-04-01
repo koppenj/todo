2afe735ce090d084bbd0fd53a4ac91b307a0b408
koppenj <joshuwakoppen@gmail.com>
2022-04-01 12:21:55 -0400

    Attaching create project to main div. Using a crude helper function to get info onto screen' '

--- src/createProject.js
+++ src/createProject.js
@@ -8,6 +8,7 @@ class Project {
 }
 
 const projectList = [];
+const display = document.querySelector('.main');
 
 function createProject() {
   const project = new Project(
@@ -19,7 +20,36 @@ function createProject() {
   projectList.push(project);
   console.log('Created New Project');
   console.table(projectList);
+  checkList();
 }
 
+
+function checkList () {
+  if (projectList.length > 0) {
+    //If theres something on the list, for each item in the array, use the helper function
+    renderList();
+    console.log('Job Complete');
+  } else {
+    console.log('Nothing to see here');
+  }
+}
+
+function renderList() {
+  //Draw the stuff to main div
+  display.replaceChildren();
+  for (const project of projectList) {
+    const card = document.createElement('details');
+    card.textContent = `${project.description}`;
+    const cardContent = document.createElement('summary');
+    cardContent.textContent = `${project.title}`;
+
+    card.appendChild(cardContent);
+    display.appendChild(card);
+  }
+}
+
+function displayToDo() {
+  //Maybe something that will expand a clicked on project to the right of the screen aka main area
+}
 
 export {createProject, projectList}
