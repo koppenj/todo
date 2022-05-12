6805950f5b4b7de772499ad473ddded9549536bb
koppenj <joshuwakoppen@gmail.com>
2022-05-12 02:45:57 -0400

    Remove date fn test from onload

--- src/index.js
+++ src/index.js
@@ -3,9 +3,7 @@ import './createProject';
 import './pageActions';
 import './task';
 import { getProjects } from './loader';
-import { test } from './calendarManager';
 
 window.onload = function() {
   getProjects();
-  test();
 }
