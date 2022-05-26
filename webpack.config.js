0fb4c1c6ee8a9ff150002fc5c37bfaa41cdc5e87
koppenj <joshuwakoppen@gmail.com>
2022-05-26 01:32:38 -0400

    [TEST] Adding Babel to existing Webpack repo

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,18 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
     ],
   },
 };
